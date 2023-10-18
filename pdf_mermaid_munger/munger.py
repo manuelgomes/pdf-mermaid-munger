7123576c7314fa5d511db4b29f043a925067db42
Manuel Gomes <mane.gomes@gmail.com>
2023-10-18 20:26:06 +0100

    Format code

--- pdf_mermaid_munger/munger.py
+++ pdf_mermaid_munger/munger.py
@@ -10,6 +10,7 @@ import pdfkit
 
 from rich import print
 
+
 class MarkdownMermaidMunger:
     def __init__(self, md_path: str, pdf_path: str | None = None) -> None:
         self.path = Path(md_path)
@@ -23,49 +24,58 @@ class MarkdownMermaidMunger:
             self.pdf_path = self.path.with_suffix(".pdf")
         if self.pdf_path.exists():
             raise FileExistsError
-        
+
     def original_images(self):
         soup = BeautifulSoup(Markdown().convert(self.path.read_text()), features="lxml")
-        return [img.get('src') for img in soup.find_all("img") if img]
-    
+        return [img.get("src") for img in soup.find_all("img") if img]
+
     def materialize(self, src: str, root: str):
         origin = self.path.parent.joinpath(src)
-        tgt = NamedTemporaryFile(dir=root, prefix=origin.name, suffix=origin.suffix, delete=False)
+        tgt = NamedTemporaryFile(
+            dir=root, prefix=origin.name, suffix=origin.suffix, delete=False
+        )
         target = Path(tgt.name)
         shutil.copyfile(src=origin.as_posix(), dst=target.as_posix())
         return target.name
-    
+
     def mermaid2png(self, source: Path, temp_md: _TemporaryFileWrapper) -> None:
         subprocess.run(
             [
-                "npx", "-p", "@mermaid-js/mermaid-cli", "mmdc", 
-                f"--input={source.as_posix()}", 
+                "npx",
+                "-p",
+                "@mermaid-js/mermaid-cli",
+                "mmdc",
+                f"--input={source.as_posix()}",
                 f"--output={temp_md.name}",
-                f"--outputFormat=png"
+                f"--outputFormat=png",
             ],
-            capture_output=True
+            capture_output=True,
         ).check_returncode()
-        
+
     def munge(self) -> str:
         dir = mkdtemp()
         try:
             originals = self.original_images()
-            source = shutil.copyfile(src=self.path, dst=Path(dir).joinpath(self.path.name))
+            source = shutil.copyfile(
+                src=self.path, dst=Path(dir).joinpath(self.path.name)
+            )
             temp_md = NamedTemporaryFile(dir=dir, delete=False, suffix=".md")
             self.mermaid2png(source=source, temp_md=temp_md)
-            
-            soup = BeautifulSoup(Markdown().convert(Path(temp_md.name).read_text()), features="lxml")
-            for img in soup.find_all('img'):
-                src = img.get('src')
-                if  src in originals:
-                    img['src'] = self.materialize(src, dir)
 
-            tmp_html  = Path(temp_md.name).with_suffix(".html")
+            soup = BeautifulSoup(
+                Markdown().convert(Path(temp_md.name).read_text()), features="lxml"
+            )
+            for img in soup.find_all("img"):
+                src = img.get("src")
+                if src in originals:
+                    img["src"] = self.materialize(src, dir)
+
+            tmp_html = Path(temp_md.name).with_suffix(".html")
             with tmp_html.open("+w") as fp:
                 fp.write(str(soup))
             pdfkit.from_file(
-                input=tmp_html.as_posix(), 
-                output_path=self.pdf_path.as_posix() ,
+                input=tmp_html.as_posix(),
+                output_path=self.pdf_path.as_posix(),
                 options={"enable-local-file-access": True},
             )
         finally:
